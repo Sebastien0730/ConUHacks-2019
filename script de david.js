c5d6d0df268a13b48e49b88ce7c477b46ec9680c
avidThaid <45408503+avidThaid@users.noreply.github.com>
2019-01-26 15:42:01 -0500

    bmi v2

--- script de david.js
+++ script de david.js
@@ -1,6 +1,8 @@
-function bmi() {
+function calculateBmi() {
     var weightInput = document.getElementById("weight").value;
     var heightInput = document.getElementById("height").value;
-    var BMI = weightInput/(heightInput*heightInput);
-    return BMI;
+    var bmi = weightInput/(heightInput*heightInput);
 }
+
+x = calculateBmi.bmi();
+document.getElementById("bmiOutput").innerHTML = x;
