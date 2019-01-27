a6fe7bab6443be5c406a6c14c2812b51943e4570
Sebastien0730 <43256461+Sebastien0730@users.noreply.github.com>
2019-01-27 00:29:48 -0500

    nice

--- script de david.js
+++ script de david.js
@@ -8,23 +8,22 @@ var type = document.getElementById("type");
 var bodyParts = document.getElementById("bodyParts");
 
 
-
 function hide() {
-    start.style.display = "none";
+    var start = document.getElementById("start");
+    var line = document.getElementById("line");
+    var heightSelec = document.getElementById("heightSelec");
+    var weightSelec = document.getElementById("weightSelec");
+    var type = document.getElementById("type");
+    var bodyParts = document.getElementById("bodyParts");
+
     line.style.display = "none";
-    heightSelec.style.display = "none";
     weightSelec.style.display = "none";
     type.style.display = "none";
     bodyParts.style.display = "none";
 
 }
 
-function start() {
-    open.style.display = "none"
-    start.style.display = "block";
-    heightSelec.style.display = "block";
 
-}
 
 function storeH() {
     var heightSelec = document.getElementById("heightSelec");
@@ -40,21 +39,22 @@ function storeH() {
 function storeW() {
 
     var weightSelec = document.getElementById("weightSelec");
+
     var type = document.getElementById("type");
     var line = document.getElementById("line");
 
     weightInput = document.getElementById("weight").value;
 
     // Output of bmi
-    document.getElementById("output").innerHTML = weightInput / (heightInput * heightInput);
+    //document.getElementById("output").innerHTML = weightInput / (heightInput * heightInput);
 
     weightSelec.style.display = "none";
-    line.style.display = "block";
     type.style.display = "block";
 
 }
 
 function showMuscle() {
     var bodyParts = document.getElementById("bodyParts");
+    line.style.display = "block";
     bodyParts.style.display = "block";
 }
